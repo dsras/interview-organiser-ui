40991e6e6738c9c1e2359591c0c2d3875538b468
Daniel Jones WSL <lis4dyslexia@gmail.com>
2022-03-29 17:37:48 +0100

    buttons to build from, one component deleted no issues

--- angular/src/app/components/calendar/calendar.component.ts
+++ angular/src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calendar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  redirect(page: string) : void {
+    this.router.navigate([page]);
   }
 
 }
--- angular/src/app/components/dashboard/dashboard.component.ts
+++ angular/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  redirect(page: string) : void {
+    this.router.navigate([page]);
   }
 
 }
