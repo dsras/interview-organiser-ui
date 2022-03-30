851bc97f5bd854b06d3abc30dac7c099a49a95d6
Sulkyoptimism <thorfinnm@yahoo.co.uk>
2022-03-30 15:50:37 +0100

    Added the grid code again

---
+++ angular/src/app/components/calendar/module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { CalendarComponent } from './calendar.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+  ],
+  declarations: [CalendarComponent],
+  exports: [CalendarComponent],
+})
+export class DemoModule {}
