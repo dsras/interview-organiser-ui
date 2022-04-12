c14d3a4f9b59bc791e9c2846234e185b2b00d237
Daniel Jones WSL <lis4dyslexia@gmail.com>
2022-04-12 09:50:51 +0100

    Calendar forms now lie within calendar component directory

--- angular/src/app/components/calendar/view-availability-modal/view-availability-modal.component.ts
+++ angular/src/app/components/calendar/view-availability-modal/view-availability-modal.component.ts
@@ -3,7 +3,7 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { CalendarEvent } from 'angular-calendar';
 import { MDBModalRef, MDBModalService } from 'ng-uikit-pro-standard';
 import { Subject } from 'rxjs';
-import { AvailabilityFormComponent } from '../../forms/availability-form/availability-form.component';
+import { AvailabilityFormComponent } from '../availability-form/availability-form.component';
 
 
 @Component({
