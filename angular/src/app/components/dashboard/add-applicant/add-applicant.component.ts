aa99b9ab66400edb2f7a67364162d26361c40240
Daniel Jones WSL <lis4dyslexia@gmail.com>
2022-04-20 11:54:48 +0100

    Form template complete

--- angular/src/app/components/dashboard/add-applicant/add-applicant.component.ts
+++ angular/src/app/components/dashboard/add-applicant/add-applicant.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, TemplateRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { RequestCenterService } from '../../requester/request-center.service';
+import { skills } from '../../requester/requestBodyTypes/types';
 
 @Component({
   selector: 'add-applicant',
@@ -12,6 +13,12 @@ export class AddApplicantComponent implements OnInit {
 
   modalRef?: BsModalRef
 
+  skillsAvailable: skills[] = [];
+
+  skillNamesAvailable: Set<string> = new Set<string>();
+
+  skillLevels: Set<string> = new Set<string>();
+
   addApplicantForm: FormGroup = this.fb.group({
     name: this.fb.group({
       firstName: ['', Validators.required],
@@ -19,7 +26,10 @@ export class AddApplicantComponent implements OnInit {
     }),
     email: ['', Validators.required, Validators.email],
     mobile: ['', Validators.minLength(3)],
-    skill: [''],    
+    skill: this.fb.group({
+      skillName: [''],
+      skillLevel: [''],
+    }),    
   })
 
   constructor(
@@ -29,13 +39,15 @@ export class AddApplicantComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    // TODO maybe put in parent as an input/output relationship
+    this.rs.getAllSkills(this.skillsAvailable, this.skillNamesAvailable, this.skillLevels );
   }
 
   openModal(template: TemplateRef<any>) {
     this.modalRef = this.ms.show(template);
   }
 
-  
+
   onSubmit(f: FormGroup) {
     this.addApplicantForm.setValue(f.value)
     //TODO this.rs.addApplicant(f.value.attribute, **) 
