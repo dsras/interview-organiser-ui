4da93b0e573b3117815958cab37981f7f40ff1b3
Sulkyoptimism <thorfinnm@yahoo.co.uk>
2022-04-26 16:42:54 +0100

    added a redundant dom method to create text components

--- angular/src/app/components/calendar/view-availability-modal/view-availability.component.ts
+++ angular/src/app/components/calendar/view-availability-modal/view-availability.component.ts
@@ -32,6 +32,70 @@ export class ViewAvailabilityModalComponent implements OnInit {
   }
 
   static addEventRef(events: CalendarEvent [], interviews: CalendarEvent[]){
+
+    var container = document.createElement('div');
+
+    events.forEach(appointment => {
+      console.log(events.length);
+      var start = appointment.start;
+      var end = appointment.end;
+
+      var outerDiv = document.createElement('div');
+      outerDiv.style.display = "flex";
+      outerDiv.style.flexDirection = 'row';
+      
+      var textArea = document.createElement('p');
+      textArea.textContent += start.toLocaleString().substring(12);
+      textArea.textContent += " -> " + end?.toLocaleString().substring(12) + '\n';
+
+      var buttonDiv = document.createElement('div');
+      buttonDiv.style.paddingLeft = '10%';      
+
+      var confirmButton = document.createElement('button');
+      confirmButton.textContent = "Confirm interview";
+
+      buttonDiv.append(confirmButton);
+      outerDiv.append(textArea);
+      outerDiv.append(buttonDiv);
+      container.append(outerDiv);
+    });
+
+    var interviewContainer = document.createElement('div');
+
+    interviews.forEach(appointment => {
+      console.log(interviews.length);
+      var start = appointment.start;
+      var end = appointment.end;
+
+      var outerDiv = document.createElement('div');
+      outerDiv.style.display = "flex";
+      outerDiv.style.flexDirection = 'row';
+      
+      var textArea = document.createElement('p');
+      textArea.textContent += start.toLocaleString().substring(12);
+      textArea.textContent += " -> " + end?.toLocaleString().substring(12) + '\n';
+
+      var buttonDiv = document.createElement('div');
+      buttonDiv.style.paddingLeft = '10%';      
+
+      var confirmButton = document.createElement('button');
+      confirmButton.textContent = "Confirm interview";
+
+      buttonDiv.append(confirmButton);
+      outerDiv.append(textArea);
+      outerDiv.append(buttonDiv);
+      interviewContainer.append(outerDiv);
+    });
+
+    const availabilityOutput = document.getElementById("availabilityOutput");
+    const interviewoutput = document.getElementById("interviewOutput");
+
+    availabilityOutput?.append(container);
+    interviewoutput?.append(interviewContainer);
+
+  }
+
+  static addEventRef_(events: CalendarEvent [], interviews: CalendarEvent[]){
     this.availability = events;
     this.interviews = interviews;
     const availabilityOutput = document.getElementById("availabilityOutput");
@@ -40,11 +104,19 @@ export class ViewAvailabilityModalComponent implements OnInit {
     const interviewoutput = document.getElementById("interviewOutput");
     const interviewList = document.createElement('p');
     interviewList.textContent += this.availability[0].start.toLocaleString().substring(0,10) + "\n";
+    var outerDiv = document.createElement('div');
+    outerDiv.style.display = "flex";
+    outerDiv.style.flexDirection = 'row';
+    var confirmButton = document.createElement('button');
+    var buttonDiv = document.createElement('div');
+    buttonDiv.style.paddingLeft = '10%';
+    confirmButton.textContent = "Confirm interview";
 
     events.forEach(appointment => {
       console.log(events.length);
       var start = appointment.start;
       var end = appointment.end;
+      
       availabilityList.textContent += start.toLocaleString().substring(12);
       availabilityList.textContent += " -> " + end?.toLocaleString().substring(12) + '\n';
       
@@ -59,7 +131,10 @@ export class ViewAvailabilityModalComponent implements OnInit {
       interviewList.textContent += " -> " + end?.toLocaleString().substring(12) + '\n';
       
     });
-    availabilityOutput?.append(availabilityList);
+    outerDiv.append(availabilityList);
+    buttonDiv.append(confirmButton);
+    outerDiv.append(buttonDiv);
+    availabilityOutput?.append(outerDiv);
     interviewoutput?.append(interviewList);
   }
 
