aa0f9ae6bf997842ba366ed7345260712da95acb
Daniel Jones WSL <lis4dyslexia@gmail.com>
2022-05-06 12:07:52 +0100

    eroor correction on imports

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -6,7 +6,7 @@ import { CreateInterviewComponent } from './create-interview/create-interview.co
 import { AllInterviewsComponent } from './all-interviews/all-interviews.component';
 import { SharedModule } from '../../shared/shared-module.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from 'src/app/app-routing.module';
+import { AppRoutingModule } from 'src/app/router/app-routing.module';
 
 
 
