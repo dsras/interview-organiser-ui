45c5cad8b60f381b43e20207bd8cec2a1c89432d
Sulkyoptimism <thorfinnm@yahoo.co.uk>
2022-05-13 16:46:34 +0100

    Started on header tests

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
@@ -27,6 +27,7 @@ describe('HeaderComponent', () => {
   let fixture: ComponentFixture<HeaderComponent>;
   const CLIENT_ID = (prodEnv) ? APPCONSTANTS.SSO_CONSTANTS.CLIENT_ID_PROD : APPCONSTANTS.SSO_CONSTANTS.CLIENT_ID_DEV;
 
+  let dService: DataSourceService;
   
 
   beforeEach(async () => {
@@ -43,8 +44,8 @@ describe('HeaderComponent', () => {
         DatePipe,
         FormBuilder, 
         SocialAuthService,
-        //DataSourceService,
-        {provide: DataSourceService, useValue: FakeDataSource},
+        DataSourceService,
+        //{provide: DataSourceService, useValue: FakeDataSource},
 
         {
           provide: 'SocialAuthServiceConfig',
@@ -52,10 +53,8 @@ describe('HeaderComponent', () => {
               autoLogin: false,
               providers: [
                   {
-                      id: GoogleLoginProvider.PROVIDER_ID,
-                      provider: new GoogleLoginProvider(CLIENT_ID)
-                      //provider: new GoogleLoginProvider('61469011957-etn1feijdcp2ap3af8sh6ri9pkf9v444.apps.googleusercontent.com')
-                          
+                    id: GoogleLoginProvider.PROVIDER_ID,
+                    provider: new GoogleLoginProvider(CLIENT_ID)                          
                   }
               ]
           } as SocialAuthServiceConfig,    
@@ -68,11 +67,40 @@ describe('HeaderComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
+    dService = TestBed.inject(DataSourceService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  //! data source service disconnected from private member variable internal to component
+  it("ngOnInit calls relevent methods", fakeAsync(() => {
+    // //attempt 1
+    // let spy = spyOn(dService, "getDataSource").and.returnValue(<Observable<any>><unknown>"login");
+
+    // component.ngOnInit();
+    // tick(3);
+
+    // expect(spy).toHaveBeenCalled();
+
+    // //attempt 2
+    // component.initService();
+    // component.updateService('route', 'login');
+    // component.ngOnInit();
+    // tick(3);
+    // expect(component.isHeader == false).toBeTruthy();
+    
+    // component.updateService('route', 'cal');
+    // component.ngOnInit();
+    // tick(3);
+    // expect(component.isHeader == true).toBeTruthy();
+    
+  }));
+
+  it('menu change calls navigate and update, and updates source variables', () => {
+    
   });
 });
