eb10256913a68f4fa970d71e4e27e0450a26b3f4
Sulkyoptimism <thorfinnm@yahoo.co.uk>
2022-05-17 17:01:21 +0100

    trying to make more comprehensive requester tests

--- src/app/services/requester/availability-requester.service.spec.ts
+++ src/app/services/requester/availability-requester.service.spec.ts
@@ -1,11 +1,12 @@
 import { DatePipe } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CalendarEvent, CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { BsModalService } from 'ngx-bootstrap/modal';
+import { Observable } from 'rxjs';
 import { availability } from '../../shared/models/types';
 
 import { AvailabilityRequesterService } from './availability-requester.service';
@@ -17,6 +18,21 @@ const AvailabilityInfoFake: availability = {
   start_time: new Date().toString(),
   end_time: new Date().toString(),
 }
+
+const FakeAvailabilityReturn: availability[] = [
+  {
+    availability_id: 0,
+    date: new Date().toString(),
+    start_time: new Date().toString(),
+    end_time: new Date().toString(),
+  },
+  {
+    availability_id: 0,
+    date: new Date().toString(),
+    start_time: new Date().toString(),
+    end_time: new Date().toString(),
+  },
+]
 
 describe('AvailabilityRequesterService', () => {
   let service: AvailabilityRequesterService;
@@ -76,24 +92,36 @@ describe('AvailabilityRequesterService', () => {
   });
 
 
-  it('getMyAvailability calls requester methods', () => {
+  it('getMyAvailability calls requester methods', fakeAsync(() => {
     let events: CalendarEvent[] = [];
     let userName: string = "";
     spy = spyOn(rService, 'getRequest').and.callThrough();
     service.getMyAvailability(events, userName);
+    tick(3);
     expect(spy).toHaveBeenCalled();
-  });
+  }));
+
+  it('getAvailabilityAll calls the requester methods', fakeAsync(() => {
+    let events: CalendarEvent[] = [];
+    spy = spyOn(rService, 'getRequest').and.returnValue(<Observable<any>><unknown>FakeAvailabilityReturn);
+    service.getAllAvailability(events);
+    tick(3);
+    expect(spy).toHaveBeenCalled();
+    expect(events.length != 0).toBeTruthy();
+  }));
   
-  it('getAvailabilityOnSkill calls requester methods', () => {
+  it('getAvailabilityOnSkill calls requester methods', fakeAsync(() => {
     let numbers: number[] = [];
     spy = spyOn(rService, 'getRequest').and.callThrough();
     service.getAvailabilityOnSkill(numbers);
+    tick(3);
     expect(spy).toHaveBeenCalled();
-  });
+  }));
     
-  it('getAvailabilityByRange calls requester methods', () => {
+  it('getAvailabilityByRange calls requester methods', fakeAsync(() => {
     spy = spyOn(rService, 'postRequestNoType').and.callThrough();
     service.getAvailabilityByRange(new Date().toString(), new Date().toString(), new Date().toString(), new Date().toString(),[1],[]);
+    tick(3);
     expect(spy).toHaveBeenCalled();
-  });
+  }));
 });
