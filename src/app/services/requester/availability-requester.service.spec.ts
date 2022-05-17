6e6c97c7aa02a26b607df4cd331e06b1cdadec1f
Sulkyoptimism <thorfinnm@yahoo.co.uk>
2022-05-17 16:39:37 +0100

    Availability requester tests fixed

--- src/app/services/requester/availability-requester.service.spec.ts
+++ src/app/services/requester/availability-requester.service.spec.ts
@@ -9,10 +9,19 @@ import { BsModalService } from 'ngx-bootstrap/modal';
 import { availability } from '../../shared/models/types';
 
 import { AvailabilityRequesterService } from './availability-requester.service';
+import { Requester } from './requester.service';
+
+const AvailabilityInfoFake: availability = {
+  availability_id: 0,
+  date: new Date().toString(),
+  start_time: new Date().toString(),
+  end_time: new Date().toString(),
+}
 
 describe('AvailabilityRequesterService', () => {
   let service: AvailabilityRequesterService;
   let spy;
+  let rService: Requester;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,57 +38,62 @@ describe('AvailabilityRequesterService', () => {
       ],
     });
     service = TestBed.inject(AvailabilityRequesterService);
+    rService = TestBed.inject(Requester);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
-
   it('date should be formatted to YYYY-MM-DD', () => {
     let tempDate = new Date('1995-12-17T03:24:00');
     expect(service.dateToStringDate(tempDate) === "1995-12-17").toBeTruthy();
   });
-
   it('time should be formatted to HH:MM', () => {
     let tempDate = new Date('1995-12-17T03:24:00');
     expect(service.dateToStringTime(tempDate) === "03:24").toBeTruthy();
   });
 
-  it('parseAvailabilityEvent gets called', () => {
-    let interviewerID: availability= new availability(0,"","","");
-    spy = spyOn(service, 'parseAvailabilityEvent').and.callThrough();
-    service.parseAvailabilityEvent(interviewerID);
-    expect(service.parseAvailabilityEvent).toHaveBeenCalled();
-  });
 
-  it('addAvailability gets called', () => {
+  it('addAvailability calls requester methods', () => {
     let first: string = "1995-12-17T03:24:00";
     let last: string = "1995-12-19T03:24:00";
     let start: string = "1995-12-17T09:24:00";
     let end: string = "1995-12-19T12:24:00";
-    spy = spyOn(service, 'addAvailability').and.callThrough();
+    spy = spyOn(rService, 'postRequest').and.callThrough();
     service.addAvailability(first, last, start, end);
-    expect(service.addAvailability).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
   });
 
-  it('getMyAvailability gets called', () => {
+
+  it('parseAvailabilityEvent formats correctly', () => {
+    let retObj = service.parseAvailabilityEvent(AvailabilityInfoFake);
+    expect(retObj.id === AvailabilityInfoFake.availability_id).toBeTruthy();
+
+    const start = new Date(AvailabilityInfoFake.date);
+    const times1 = AvailabilityInfoFake.start_time.split(':');
+    start.setHours(parseInt(times1[0]), parseInt(times1[1]));
+    expect(retObj.start.toString() === start.toString()).toBeTruthy();
+  });
+
+
+  it('getMyAvailability calls requester methods', () => {
     let events: CalendarEvent[] = [];
     let userName: string = "";
-    spy = spyOn(service, 'getMyAvailability').and.callThrough();
+    spy = spyOn(rService, 'getRequest').and.callThrough();
     service.getMyAvailability(events, userName);
-    expect(service.getMyAvailability).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
   });
   
-  it('getAvailabilityOnSkill gets called', () => {
+  it('getAvailabilityOnSkill calls requester methods', () => {
     let numbers: number[] = [];
-    spy = spyOn(service, 'getAvailabilityOnSkill').and.callThrough();
+    spy = spyOn(rService, 'getRequest').and.callThrough();
     service.getAvailabilityOnSkill(numbers);
-    expect(service.getAvailabilityOnSkill).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
   });
     
-  it('getAvailabilityByRange gets called', () => {
-    spy = spyOn(service, 'getAvailabilityByRange').and.callThrough();
+  it('getAvailabilityByRange calls requester methods', () => {
+    spy = spyOn(rService, 'postRequestNoType').and.callThrough();
     service.getAvailabilityByRange(new Date().toString(), new Date().toString(), new Date().toString(), new Date().toString(),[1],[]);
-    expect(service.getAvailabilityByRange).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalled();
   });
 });
