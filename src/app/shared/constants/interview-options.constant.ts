c271b39f381f381b783854b3d3a738183d497123
Daniel Jones WSL <lis4dyslexia@gmail.com>
2022-05-23 16:34:52 +0100

    interview options constants refactored

--- src/app/shared/constants/interview-options.constant.ts
+++ src/app/shared/constants/interview-options.constant.ts
@@ -1,21 +1,11 @@
-export class InterviewOptions {
-  private static _interviewStatus: Array<string> = [
-    'Completed',
-    'Candidate No Show',
-    'Panel No Show',
-  ];
+export const interviewStatusOptions: Array<string> = [
+  'Completed',
+  'Candidate No Show',
+  'Panel No Show',
+];
 
-  private static _interviewOutcome: Array<string> = [
-    'Progressed',
-    'Didnt Progress',
-    'Hired',
-  ];
-
-  static getStatus(): Array<string> {
-    return InterviewOptions._interviewStatus;
-  }
-
-  static getOutcome(): Array<string> {
-    return InterviewOptions._interviewOutcome;
-  }
-}
+export const interviewOutcomeOptions: Array<string> = [
+  'Progressed',
+  'Didnt Progress',
+  'Hired',
+];
