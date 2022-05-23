95f90a4c097a6d1666a62f46907900e498c25048
Sulkyoptimism <thorfinnm@yahoo.co.uk>
2022-05-23 16:21:55 +0100

    Name change fix

--- src/app/shared/models/types.spec.ts
+++ src/app/shared/models/types.spec.ts
@@ -3,17 +3,17 @@ import * as types from './types';
 
 describe('model classes pass init test', () => {
     let skillIdOnly: types.skillIdOnly;
-    let statusUpdate: types.statusUpdate;
-    let userData: types.userData;
+    let statusUpdate: types.StatusUpdate;
+    let userData: types.UserData;
     let Skills: types.Skills;
-    let available: types.available;
+    let available: types.Available;
     let applicant: types.applicant;
-    let availabilityForInterviews: types.availabilityForInterviews;
-    let availability: types.availability;
-    let availabilityRange: types.availabilityRange;
-    let interviewRange: types.interviewRange;
-    let interview: types.interview;
-    let interviewReturn: types.interviewReturn;
+    let availabilityForInterviews: types.AvailabilityForInterviews;
+    let availability: types.Availability;
+    let availabilityRange: types.AvailabilityRange;
+    let interviewRange: types.InterviewRange;
+    let interview: types.Interview;
+    let interviewReturn: types.InterviewReturn;
     let SkillOptions: types.SkillOptions;
     let AppRoles: types.AppRoles;
 
@@ -41,17 +41,17 @@ describe('model classes pass init test', () => {
         ]
     });
     skillIdOnly = new types.skillIdOnly(10);
-    statusUpdate = new types.statusUpdate(10, 'Confirmed');
-    userData = new types.userData(10, 'name', 'password', 'test@email.com', '08001000100', 'username', 'business title', 'account', 'business unit', 'join date', 'designation', 'location', 'apriori', [new types.AppRoles(10, 'desc', 'name')]);
+    statusUpdate = new types.StatusUpdate(10, 'Confirmed');
+    userData = new types.UserData(10, 'name', 'password', 'test@email.com', '08001000100', 'username', 'business title', 'account', 'business unit', 'join date', 'designation', 'location', 'apriori', [new types.AppRoles(10, 'desc', 'name')]);
     Skills = new types.Skills(10, 'name', 'level');
-    available = new types.available('date1', 'time1', 'time2', [1,2,3]);
+    available = new types.Available('date1', 'time1', 'time2', [1,2,3]);
     applicant = new types.applicant('name', 'name2', 'email', '08001000100', 1);
-    availabilityForInterviews = new types.availabilityForInterviews('interviewer', 0, 0, 'date', 'time1', 'time2');
-    availability = new types.availability(10, 'date', 'time1', 'time2');
-    availabilityRange = new types.availabilityRange('date1', 'date2', 'time1', 'time2');
-    interviewRange = new types.interviewRange('date1', 'date2', 'time1', 'time2', [0,1,2]);
-    interview = new types.interview([10,7,2], 'date1', 'time1', 'time2', 'additional');
-    interviewReturn = new types.interviewReturn(10, ['me','you','them'], 'date1', 'time1', 'time2', 'additional', 'status', 'outcome');
+    availabilityForInterviews = new types.AvailabilityForInterviews('interviewer', 0, 0, 'date', 'time1', 'time2');
+    availability = new types.Availability(10, 'date', 'time1', 'time2');
+    availabilityRange = new types.AvailabilityRange('date1', 'date2', 'time1', 'time2');
+    interviewRange = new types.InterviewRange('date1', 'date2', 'time1', 'time2', [0,1,2]);
+    interview = new types.Interview([10,7,2], 'date1', 'time1', 'time2', 'additional');
+    interviewReturn = new types.InterviewReturn(10, ['me','you','them'], 'date1', 'time1', 'time2', 'additional', 'status', 'outcome', 'organizer');
     SkillOptions = new types.SkillOptions((<Set<string>><unknown>['skill1', 'skill2', 'skill3']), (<Set<string>><unknown>['level1', 'level2', 'level3']));
     AppRoles = new types.AppRoles(10, 'desc', 'name');
   });
