aec5b093d50c781ebed50bc16a443a5202229bb2
Sulkyoptimism <thorfinnm@yahoo.co.uk>
2022-05-24 16:25:47 +0100

    More testing fixes

--- src/app/services/mat-dialog.service.spec.ts
+++ src/app/services/mat-dialog.service.spec.ts
@@ -1,4 +1,12 @@
+import { DatePipe } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 import { MatDialogService } from './mat-dialog.service';
 
@@ -6,7 +14,22 @@ describe('MatDialogService', () => {
   let service: MatDialogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports:[
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+        MatDialogModule,
+      ],
+      providers: [
+        BsModalService,
+        DatePipe,
+        FormBuilder,       
+        //ModalControllerService,
+        MatDialog,
+      ],
+    });
     service = TestBed.inject(MatDialogService);
   });
 
