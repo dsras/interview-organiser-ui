999d9663f193180890e8a701058de44bdf8c16fa
Sulkyoptimism <thorfinnm@yahoo.co.uk>
2022-06-15 14:35:24 +0100

    Import error fix

--- src/app/router/guards/login.guard.ts
+++ src/app/router/guards/login.guard.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { LoginComponent } from 'src/app/components/login/login.component';
-import { GetUserDataService } from 'src/app/services/get-user-data.service';
+import { GetUserDataService } from '../../services/get-user-data.service';
 import { RequestCenterService } from 'src/app/services/requester/request-center.service';
 
 @Injectable({
